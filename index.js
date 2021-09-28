78490e010bf9d3394a9b76fee53ea1c023db2eb1
Michell Alegría <alegria.trm@gmail.com>
2021-09-28 15:26:46 -0500

    add new rules to eslint configuration

--- index.js
+++ index.js
@@ -1,73 +1,80 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-      jest: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jsx-a11y", "import", "react-hooks", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/strict",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "jsx-a11y", "import", "react-hooks", "react"],
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:jsx-a11y/strict",
-      "plugin:react/recommended",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "plugin:import/typescript",
-      "prettier",
+  },
+  rules: {
+    "eol-last": [
+      "error",
+      "always"
     ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always-and-inside-groups",
+      },
+    ],
+    "import/exports-last": "warn",
+    "indent": [
+      "error",
+      2
+    ],
+    "max-lines": [
+      "error",
+      {
+        max: 500,
+        skipBlankLines: true,
+      },
+    ],
+    "max-params": ["warn", 3],
+    "no-alert": "error",
+    "no-extend-native": "error",
+    "no-return-await": "error",
+    "no-sync": "error",
+    "no-throw-literal": "error",
+    "no-useless-return": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-destructuring": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "require-await": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
       },
     },
-    rules: {
-      "import/order": [
-        "warn",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-          ],
-          "newlines-between": "always-and-inside-groups",
-        },
-      ],
-      "import/exports-last": "warn",
-      "max-lines": [
-        "error",
-        {
-          max: 500,
-          skipBlankLines: true,
-        },
-      ],
-      "max-params": ["warn", 3],
-      "no-alert": "error",
-      "no-extend-native": "error",
-      "no-return-await": "error",
-      "no-sync": "error",
-      "no-throw-literal": "error",
-      "no-useless-return": "error",
-      "no-var": "error",
-      "prefer-const": "error",
-      "prefer-destructuring": "warn",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "require-await": "error",
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-      "import/resolver": {
-        node: {
-          paths: ["src"],
-        },
-      },
-    },
-  };
-  
+  },
+};
