aa1c3dca7697d9f55bcd09e21c6cd6f3a31ad0b4
Eder Sánchez <eder@mivest.io>
2022-02-10 13:48:37 -0600

    fix: move rules and overrides to the right file

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,19 +12,6 @@ module.exports = {
   },
   rules: {
     'prettier/prettier': ['error'],
-    "indent": "off",
-    // Disable explicit-function-return-type for all file types (JS, TS, etc)
-    // It will be enabled below only for TS files.
-    // https://github.com/typescript-eslint/typescript-eslint/issues/964
-    "@typescript-eslint/explicit-function-return-type": "off"
-  },
-  overrides: [
-    // Enable explicit-function-return-type only for TS files
-    {
-      "files": ["*.ts", "*.tsx"],
-      "rules": {
-        "@typescript-eslint/explicit-function-return-type": ["error"]
-      }
-    }
-  ]
+    indent: 'off'
+  }
 }
--- index.js
+++ index.js
@@ -72,7 +72,20 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'require-await': 'error',
     indent: 'off',
+    // Disable explicit-function-return-type for all file types (JS, TS, etc)
+    // It will be enabled below only for TS files.
+    // https://github.com/typescript-eslint/typescript-eslint/issues/964
+    "@typescript-eslint/explicit-function-return-type": "off"
   },
+  overrides: [
+    // Enable explicit-function-return-type only for TS files
+    {
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        "@typescript-eslint/explicit-function-return-type": ["error"]
+      }
+    }
+  ],
   settings: {
     react: {
       version: 'detect',
