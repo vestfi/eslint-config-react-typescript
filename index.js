71449921c29ccd56a920fda1b584c2a604127ed1
Diego Lara <dyegolara@icloud.com>
2022-06-29 13:42:31 -0500

    fix: run prettier

--- index.js
+++ index.js
@@ -75,16 +75,16 @@ module.exports = {
     // Disable explicit-function-return-type for all file types (JS, TS, etc)
     // It will be enabled below only for TS files.
     // https://github.com/typescript-eslint/typescript-eslint/issues/964
-    "@typescript-eslint/explicit-function-return-type": "off"
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
   overrides: [
     // Enable explicit-function-return-type only for TS files
     {
-      "files": ["*.ts", "*.tsx"],
-      "rules": {
-        "@typescript-eslint/explicit-function-return-type": ["error"]
-      }
-    }
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+      },
+    },
   ],
   settings: {
     react: {
